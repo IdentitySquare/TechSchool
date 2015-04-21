4451b146cbecbf28e8eef66a48020e66d986683d
danielpaul <danielpaul2008@gmail.com>
2015-04-21 20:13:11 +0100

    added image

--- js/main.js
+++ js/main.js
@@ -17,8 +17,7 @@
     	// will fade out the whole DIV that covers the website. 
     	$("#preloader").delay(500).fadeOut("slow").remove();     
       
-    	$('.js #hero .hero-image img').addClass("animated fadeInUpBig"); 
-      $('.js #hero .buttons a.trial').addClass("animated shake");    
+    	$('.js #hero .hero-image img').addClass("animated fadeInUpBig");   
 
   	}) 
 
